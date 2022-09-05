4802ff916c3b72d576da241a2ce3cf5185b0b662
Muharromi Putra Ardhana <muharromi42@gmail.com>
2022-09-05 11:02:26 +0700

    menambahkan jam digital dan AM PM

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,55 @@ const clock = () =>{
     seconds.style.transform = `rotateZ(${ss}deg)`
 }
 setInterval(clock, 1000) // 1000 = 1s
+
+// clock and date text
+
+const texthour = document.getElementById('text-hour'),
+      textminutes = document.getElementById('text-minutes'),
+      textAmPm = document.getElementById('text-ampm'),
+      dateDay = document.getElementById('date-day'),
+      dateMonth = document.getElementById('date-month'),
+      dateYear = document.getElementById('date-year')
+
+const clockText = () => {
+    let date = new Date ()
+
+    let hh = date.getHours(),
+        ampm,
+        mm = date.getMinutes(),
+        day = date.getDate(),
+        month = date.getMonth(),
+        year = date.getFullYear()
+
+    
+
+// change the hours from 24 to 12 hours 
+if(hh >= 12){
+    hh = hh - 12
+    ampm = 'PM'
+}else {
+    ampm = 'AM'
+}
+// when its 0 AM and transform to 12 AM
+if(hh == 0){
+    hh = 12
+}
+// show a zero before hours
+if(hh < 10){
+    hh = `0${hh}`
+}
+// show time
+texthour.innerHTML = `${hh}:`
+
+// show a zero before the minutes
+if(mm < 10){
+    mm = `0${mm}`
+}
+
+// show minutes
+textminutes.innerHTML = mm
+
+// show AM PM
+textAmPm.innerHTML = ampm
+}
+setInterval(clockText, 1000) //1000 = 1s
